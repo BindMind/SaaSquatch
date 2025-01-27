5ad40bfaab9df4a8710d51e8dd9ccea4c7f4495c
BindMind <alexbindmind@gmail.com>
2025-01-26 23:33:27 -0800

    Initial implementation.

---
+++ README.md
@@ -0,0 +1,7 @@
+## Running this Extraordinary Picocli Project 
+1. Build using Gradle (`.\gradlew.bat build` on Windows or `gradew build` on Linux)
+2. Navigate to `app\build\distributions\`
+3. Unpack the `.tar` or `.zip`
+4. Navigate to `SaasQuatch\bin`
+5. Run the `saasquatch.bat` script in command line
+6. Enjoy :)
---
+++ app/src/main/java/saasquatch/CoolName.java
@@ -0,0 +1,26 @@
+/*
+ * A quick program for the SaasQuatch competition.
+ */
+package saasquatch;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+
+@Command(name = "saasquatch", mixinStandardHelpOptions = true, version = "saasquatch 1.0",
+         description = "Prints the name of a neat company.")
+public class CoolName implements Callable<Integer>  {
+    @Override
+    public Integer call() throws Exception {
+        System.out.printf("SaasQuatch is cool :)");
+
+        return 0;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new CoolName()).execute(args);
+        System.exit(exitCode);
+    }
+}
